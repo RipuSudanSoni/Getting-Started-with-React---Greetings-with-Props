b806b9c7d9ced7d419f43754f4c563ed13576f40
Ripu Sudan Soni <ripu7481@gmail.com>
2021-06-18 19:10:28 +0530

    first commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+
+function welcome() {
+    return (
+        <>
+        <h1>Hey ! Ripu Sudan Soni</h1>
+        <h2>Welcome to Newton School.</h2>
+        </>
+    );
+}
+ export default welcome;
