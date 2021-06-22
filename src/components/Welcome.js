775fff4ac65797b189f22b1ddccab9f57a00dc25
Ripu Sudan Soni <ripu7481@gmail.com>
2021-06-22 13:51:56 +0530

    first comi

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,11 @@
 import React from "react";
 
 const Welcome=(props)=>{
+    let str = "Welcome to Newton School."
     return(
         <>
             <h1>Hey ! {props.name}</h1>
-            <h2>Welcome to Newton School.</h2>
+            <h2>{str}</h2>
         </> 
     );
 };
